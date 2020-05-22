b0a1a8c2a074f6b426d13ebe1f5ae4fde85233da
PepeSilvia215 <gideongoddard@protonmail.com>
2020-05-22 22:41:05 +0000

    bro whats a promise

--- commands/gif.js
+++ commands/gif.js
@@ -2,6 +2,11 @@ const { giphyKey } = require('../api-keys.json');
 const { wednesdayChannelId } = require('../config.json'); //wednesday cars channel id
 const { replyWithEmbeddedGif } = require('../utils');
 const fetch = require('node-fetch');
+
+const showGifError = function(channel, error) {
+   channel.send('bruh i wanted to find a gif for you but something went bad, i am still bad at this');
+   console.error(error);
+}
 module.exports = {
 	name: 'gif',
 	description: 'posts gifs from giphy bruh',
@@ -16,25 +21,26 @@ module.exports = {
         const query = args.join(' ');
         const queryUrl = args.join('%20');
         const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${giphyKey}&q=${queryUrl}&limit=1&offset=${offset}&rating=PG-13&lang=en`;
-        try {
-            const response = await fetch(endpoint, {cache: 'no-cache'});
-            if (!response.ok) {
-                throw new Error('giphy request failed');
-                return;
+        fetchOptions = { cache: 'no-cache' };
+        fetch(endpoint, fetchOptions)
+        .then(function(response) {
+            if (response.ok) {
+                return response.json();
             }
-            const jsonResponse = await response.json();
-            //get gif url to embed
+            throw new Error('giphy request failed');
+        })
+        .then(function (jsonResponse) {
             const gifEmbedUrl = jsonResponse['data'][0]['images']['original']['url'];
+            //now try to get channel
             message.client.channels.fetch(wednesdayChannelId)
-                .then(function(channel) {
-                    replyWithEmbeddedGif(channel, gifEmbedUrl, `\`${query}\``);
-                })
-                .catch(function(error) {
-                    throw error;
-                });
-        } catch (error) {
-            message.channel.send('bruh i wanted to find a gif for you but something went bad');
-            console.error(error);
-        }
+            .then(function (wednesdayChannel) {
+                replyWithEmbeddedGif(wednesdayChannel, gifEmbedUrl, `\`${query}\``);
+            }).catch(function(error) {
+                showGifError(message.channel, error);
+            });
+        })
+        .catch(function(error) {
+            showGifError(message.channel, error);
+        });
 	}
 };
